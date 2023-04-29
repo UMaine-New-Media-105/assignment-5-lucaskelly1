c398c5533b7b28486aa46c572be6dede789e5427
lucaskelly1 <124173836+lucaskelly1@users.noreply.github.com>
2023-04-29 15:37:53 -0400

    Create challengethree.js
    https://editor.p5js.org/lucaskelly/sketches/iIXRzmjx0

---
+++ challengethree.js
@@ -0,0 +1,150 @@
+let bubbles = [];
+let donuts = [];
+let squares = [];
+
+function setup() {
+  createCanvas(960, 540);
+
+  // create 50 bubbles with random position and size
+  for (let i = 0; i < 50; i++) {
+    let x = random(width);
+    let y = random(height);
+    let radius = random(10, 30); // smaller radius
+    let speedX = random(-0.5, 0.5); // slower speed
+    let speedY = random(-0.5, 0.5); // slower speed
+    bubbles.push(new Bubble(x, y, radius, speedX, speedY));
+  }
+
+  // create 50 donuts with fixed position and random size
+  for (let i = 0; i < 50; i++) {
+    let x = random(width/4); // set x position to the left quarter of the canvas
+    let y = random(height);
+    let radius = random(10, 30); // smaller radius
+    donuts.push(new Donut(x, y, radius));
+  }
+  
+  // create 50 squares with fixed position and random size
+  for (let i = 0; i < 50; i++) {
+    let x = random(width/2, width); // set x position to the right half of the canvas
+    let y = random(height);
+    let size = random(10, 30); // smaller size
+    squares.push(new Square(x, y, size, size));
+  }
+}
+
+function draw() {
+  background(220);
+
+  // update and show bubbles
+  for (let i = 0; i < bubbles.length; i++) {
+    bubbles[i].move();
+    bubbles[i].update();
+    bubbles[i].show();
+  }
+
+  // show donuts at fixed positions on the left side of the canvas
+  for (let i = 0; i < donuts.length; i++) {
+    donuts[i].show();
+  }
+
+  // show squares at fixed positions on the right side of the canvas
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].show();
+  }
+}
+class Bubble {
+  constructor(x, y, radius, speedX, speedY) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speedX = speedX;
+    this.speedY = speedY;
+  }
+
+  move() {
+    // jitter randomly
+    this.x += random(-0.5, 0.5) + this.speedX; // slower movement
+    this.y += random(-0.5, 0.5) + this.speedY; // slower movement
+
+    // keep within bounds
+    this.x = constrain(this.x, this.radius, width - this.radius);
+    this.y = constrain(this.y, this.radius, height - this.radius);
+  }
+
+  update() {
+    // do nothing for now
+  }
+
+  show() {
+    push();
+    noStroke();
+    fill(random(255), random(255), random(255), 150);
+    let x1 = this.x - this.radius;
+    let y1 = this.y + this.radius;
+    let x2 = this.x + this.radius;
+    let y2 = this.y + this.radius;
+    let x3 = this.x;
+    let y3 = this.y - this.radius;
+    triangle(x1, y1, x2, y2, x3, y3);
+    pop();
+  }
+}
+
+class Donut {
+  constructor(x, y, radius, speedX, speedY) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speedX = speedX;
+    this.speedY = speedY;
+  }
+
+  move() {
+    // do nothing
+  }
+
+  update() {
+    // do nothing for now
+  }
+
+  show() {
+    push();
+    translate(this.x, this.y);
+    rotate(frameCount); // rotate the donut
+    stroke(0);
+    strokeWeight(4);
+    fill(255, 204, 0);
+    ellipse(0, 0, this.radius * 2);
+    fill(102, 51, 0);
+    ellipse(0, 0, this.radius * 1.2);
+    pop();
+  }
+}
+
+class Square {
+  constructor(x, y, width, height, speedX, speedY) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = width;
+    this.speedX = speedX;
+    this.speedY = speedY;
+  }
+
+  move() {
+    // do nothing
+  }
+
+  update() {
+    // do nothing for now
+  }
+
+  show() {
+    push();
+    translate(this.x, this.y);
+    stroke('red')
+    fill('orange')
+    rect(100, 100,20,20);
+    pop();
+  }
+}
